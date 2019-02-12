ce7e09e7d87b290f1c9c507a018e5c0119591af8
Zeroslope <738543046@qq.com>
2019-02-12 14:02:21 +0800

    Semantic ui setup

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import HomePage from './components/HomePage'
 import LoginPage from './components/LoginPage'
 
 const App = () => (
-  <div>
+  <div className='ui container'>
     <Route path='/' exact component={HomePage} />
     <Route path='/login' exact component={LoginPage} />
   </div>
